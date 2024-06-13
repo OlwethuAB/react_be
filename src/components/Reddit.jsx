6e2b73e904d05848bbfbf78bed8d0dff19abcc6a
OlwethuAB <olwethu.bhiya@mytalentcrm.com>
2024-06-13 14:05:00 +0200

    Randos and toggles

--- src/components/Reddit.jsx
+++ src/components/Reddit.jsx
@@ -1,52 +1,52 @@
-// import React, {useEffect, useState} from "react";
-// import useFetch from "../hooks/useFetch";
-// // const { data:posts, isLoading, errorMessage } = useFetch('https://www.reddit.com/r/Marvel/comments/pkdevy/if_anyone_can_be_spiderman_why_not_black_panther.json')
+import React, {useEffect, useState} from "react";
+import useFetch from "../hooks/useFetch";
+// const { data:posts, isLoading, errorMessage } = useFetch('https://www.reddit.com/r/Marvel/comments/pkdevy/if_anyone_can_be_spiderman_why_not_black_panther.json')
 
-// export default function Reddit() {
-//     const [posts, setPosts] = useState([]);
-//     const [isLoading, setIsLoading] = useState(true);
-//     const [errorMessage, setErrorMessage] = useState(false); // Set initial errorMessage to false
+export default function Reddit() {
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(false); // Set initial errorMessage to false
 
-//     useEffect(() => {
-//         fetch('https://www.reddit.com/r/Marvel/comments/pkdevy/if_anyone_can_be_spiderman_why_not_black_panther.json')
-//         .then(response => response.json())
-//         .then(results => {
-//             setIsLoading(false);
-//             const dataChildren = results.flatMap(item => item.data.children);
-//             setPosts(dataChildren);
-//         })
-//         .catch( error => {
-//             setIsLoading(false);
-//             setErrorMessage('There was an error fetching data from Reddit.');
-//         });
-//     }, []);
+    useEffect(() => {
+        fetch('https://www.reddit.com/r/Marvel/comments/pkdevy/if_anyone_can_be_spiderman_why_not_black_panther.json')
+        .then(response => response.json())
+        .then(results => {
+            setIsLoading(false);
+            const dataChildren = results.flatMap(item => item.data.children);
+            setPosts(dataChildren);
+        })
+        .catch( error => {
+            setIsLoading(false);
+            setErrorMessage('There was an error fetching data from Reddit.');
+        });
+    }, []);
 
-//     return (
-//         <div className="container"> 
-//             <h2>Reddit - ing </h2>
-//             <br />
-//             {isLoading && <div>loading ...</div>}
+    return (
+        <div className="container"> 
+            <h2>Reddit - ing </h2>
+            <br />
+            {isLoading && <div>loading ...</div>}
 
-//             {posts.length > 0 && ( // Check if posts array is not empty
-//                 <ul>
-//                     {posts.map(post => (
-//                         <li key={post.data.id} className="reddit">
-//                             <a href={`https://www.reddit.com${post.data.permalink}`}>
-//                                 {post.data.title}
-//                             </a>
-//                         </li>
-//                     ))}
-//                     {posts.map(post => (
-//                         <li key={post.data.id} className="reddits">
-//                             <a href={`https://www.reddit.com${post.data.permalink}`}>
-//                                 {post.data.body}
-//                             </a>
-//                         </li>
-//                     ))}
-//                 </ul>
-//             )}
+            {posts.length > 0 && ( // Check if posts array is not empty
+                <ul>
+                    {posts.map(post => (
+                        <li key={post.data.id} className="reddit">
+                            <a href={`https://www.reddit.com${post.data.permalink}`}>
+                                {post.data.title}
+                            </a>
+                        </li>
+                    ))}
+                    {posts.map(post => (
+                        <li key={post.data.id} className="reddits">
+                            <a href={`https://www.reddit.com${post.data.permalink}`}>
+                                {post.data.body}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            )}
 
-//             {errorMessage && <div>{errorMessage}</div>}
-//         </div>
-//     )
-// }
+            {errorMessage && <div>{errorMessage}</div>}
+        </div>
+    )
+}
--- src/pages/RandomSwitches.jsx
+++ src/pages/RandomSwitches.jsx
@@ -1,33 +1,33 @@
-// import React, { useState } from "react";
-// import useToggle from '../hooks/useToggle';
-// import Joke from "../components/Joke";
-// import Reddit from "../components/Reddit";
+import React, { useState } from "react";
+import useToggle from '../hooks/useToggle';
+import Joke from "../components/Joke";
+import Reddit from "../components/Reddit";
 
 
-// export default function RandomSwitches() {
-//     const [jokeVisible, setJokeVisible] = useState(false);
-//     const [redditVisible, setRedditVisible] = useToggle(false);
-//     // const [isJoke, setJoke] = useToggle();
-//     // const [isReddit, setReddit] = useToggle();
+export default function RandomSwitches() {
+    const [jokeVisible, setJokeVisible] = useState(false);
+    const [redditVisible, setRedditVisible] = useToggle(false);
+    // const [isJoke, setJoke] = useToggle();
+    // const [isReddit, setReddit] = useToggle();
 
-//     return (
-//         <div className="container">
-//             <div className="buttons">
-//                 <button onClick={() => setJokeVisible(prevJokeVisible => !jokeVisible)} className='button'  >Joke Toggle Feature</button>
-//                 <button onClick={() => setRedditVisible(prevRedditVisible => !redditVisible)} className='button'>Reddit Toggle Feature</button>
-//                 {/* <button onClick={setJoke} className='button'>Joke Toggle Feature</button>
-//                 <button onClick={setReddit} className='button'>Reddit Toggle Feature</button> */}
-//             </div>
-//             <div>
-//                 {jokeVisible && <Joke />}
-//                 {redditVisible && <Reddit />}
-//             </div>
-//             {/* <div>
-//                 {isJoke && <Joke />}
-//                 {isReddit && <Reddit />}
-//             </div> */}
-//         </div>
-//     )
-// }
+    return (
+        <div className="container">
+            <div className="buttons">
+                <button onClick={() => setJokeVisible(prevJokeVisible => !jokeVisible)} className='button'  >Joke Toggle Feature</button>
+                <button onClick={() => setRedditVisible(prevRedditVisible => !redditVisible)} className='button'>Reddit Toggle Feature</button>
+                {/* <button onClick={setJoke} className='button'>Joke Toggle Feature</button>
+                <button onClick={setReddit} className='button'>Reddit Toggle Feature</button> */}
+            </div>
+            <div>
+                {jokeVisible && <Joke />}
+                {redditVisible && <Reddit />}
+            </div>
+            {/* <div>
+                {isJoke && <Joke />}
+                {isReddit && <Reddit />}
+            </div> */}
+        </div>
+    )
+}
 
 
